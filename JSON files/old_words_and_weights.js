c9004d20f278d71eb6281b5c88106bda4d22e6e9
Isaias-Gabriel <hyzegabriel@gmail.com>
2022-07-15 12:44:18 -0300

    updates on the file old_words_and_weights.js

--- JSON files/old_words_and_weights.js
+++ JSON files/old_words_and_weights.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:463ef9019070990879676dd91a3cfe0c9ad74a979b340971f1384f643dfc87a2
-size 6185472
+oid sha256:ddd3504f0ab2474e25ef2c39dd5a46d408cef684047f43aaa41fec0c4b23e881
+size 6976053
--- JSON files/visited_wikipedia_pages.js
+++ JSON files/visited_wikipedia_pages.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0b2258354910b5cf8e32a8f627c9d6aa0232893e7caca6812e930ce9b903abc8
-size 3822642
+oid sha256:03b83e987e37ac0505749c00fec1e685bd177a25b457d885ce16f72d5bf9a1fd
+size 4656455
